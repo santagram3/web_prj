dafb136662a6619419fc0d72f9e454923074c12f
santagram3 <soslimso@naver.com>
2022-07-19 16:31:36 +0900

    페이징 , 엔드 리얼 프리브 , 등

---
+++ src/test/java/com/project/web_prj/common/paging/PageMakerTest.java
@@ -0,0 +1,26 @@
+package com.project.web_prj.common.paging;
+
+import com.project.web_prj.board.repository.BoardRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageMakerTest {
+
+    @Autowired
+    BoardRepository repository;
+
+    @Test
+    void pageInfoTest() {
+
+        int totalCount = repository.getTotalCount();
+        PageMaker pm = new PageMaker(new Page(17, 10), totalCount);
+
+        System.out.println(pm);
+
+//        assertEquals(40, pm.getEndPage());
+//        assertEquals(31, pm.getBeginPage());
+    }
+
+}
